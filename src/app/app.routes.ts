637bdb54e60343fbbdc6849ac288b6f787105e73
Alejandro Fabra <alfaseart@gmail.com>
2024-02-28 00:00:59 +0100

    update text choose

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { InitComponent } from './pages/init/init.component';
+import { TextChooseComponent } from './pages/text-choose/text-choose.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'init', component:InitComponent},
+    {path: 'choose', component:TextChooseComponent},
+    { path: '**', redirectTo: '/choose' }
+];
---
+++ src/app/pages/text-choose/text-choose.component.ts
@@ -0,0 +1,77 @@
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { log } from 'console';
+import Typed from 'typed.js';
+
+@Component({
+  selector: 'app-text-choose',
+  standalone: true,
+  imports: [],
+  templateUrl: './text-choose.component.html',
+  styleUrl: './text-choose.component.scss'
+})
+export class TextChooseComponent implements OnInit, AfterViewInit{
+
+  typed:any; 
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+
+    this.animateExplanation();
+    
+    //todo, on complete: preguntas que haces y luego das las opciones una por una. 
+  }
+
+  animateExplanation(){
+    this.typed = new Typed('#writeText1', {
+      strings: ['Despiertas en un mundo en que unos seres sintientes <br> más inteligentes que la media <br>  maltratan y asesinan a otros seres sintientes <br> sin necesidad y siendo conscientes de lo que hacen.'],
+      typeSpeed: 30,
+      showCursor:false,
+      onComplete: this.animateQuestion
+    })
+  }
+
+  animateQuestion(){
+    let that = this
+    this.typed = new Typed('#writeText2', {
+      strings: [ '¿qué haces?'],
+      typeSpeed: 70,
+      showCursor:false,
+      onComplete: that.animateAnswer1
+    })
+  }
+
+  animateAnswer1(){
+    this.typed = new Typed('#writeText3', {
+      strings: [ 'Te escandalizas e intentas hacer todo lo posible por evitar que se siga haciendo.'],
+      typeSpeed: 10,
+      showCursor:false,
+      onComplete: this.animateAnswer2
+    })
+  }
+
+  animateAnswer2(){
+    this.typed = new Typed('#writeText4', {
+      strings: [ 'No te unes a ellos, pero piensas que cada cual haga lo que quiera.'],
+      typeSpeed: 10,
+      showCursor:false,
+      onComplete: this.animateAnswer3
+    })
+  }
+
+  animateAnswer3(){
+    this.typed = new Typed('#writeText5', {
+      strings: [ 'Te unes a ellos y aprovechas esos privilegios sin darle muchas vueltas.'],
+      typeSpeed: 10,
+      showCursor:false
+    })
+  }
+
+  animateAnswers(){
+    console.log()
+  }
+
+}
