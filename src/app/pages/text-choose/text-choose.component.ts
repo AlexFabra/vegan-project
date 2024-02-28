141cbbe560a11da7a583c791d31dfe36023b3e66
Alejandro Fabra <alfaseart@gmail.com>
2024-02-28 22:37:04 +0100

    text-choose update

--- src/app/pages/text-choose/text-choose.component.ts
+++ src/app/pages/text-choose/text-choose.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { log } from 'console';
 import Typed from 'typed.js';
 
@@ -19,8 +19,10 @@ export class TextChooseComponent implements OnInit, AfterViewInit{
   }
 
   ngAfterViewInit(): void {
-
-    this.animateExplanation();
+    if (typeof document !== 'undefined') {
+      this.animateExplanation(); 
+    }
+    
     
     //todo, on complete: preguntas que haces y luego das las opciones una por una. 
   }
@@ -35,43 +37,61 @@ export class TextChooseComponent implements OnInit, AfterViewInit{
   }
 
   animateQuestion(){
-    let that = this
+
     this.typed = new Typed('#writeText2', {
       strings: [ '¿qué haces?'],
       typeSpeed: 70,
-      showCursor:false,
-      onStop: that.animateAnswer1
-    })
-  }
-
-  animateAnswer1(){
-    this.typed = new Typed('#writeText3', {
-      strings: [ 'Te escandalizas e intentas hacer todo lo posible por evitar que se siga haciendo.'],
-      typeSpeed: 10,
-      showCursor:false,
-      onComplete: this.animateAnswer2
-    })
-  }
-
-  animateAnswer2(){
-    this.typed = new Typed('#writeText4', {
-      strings: [ 'No te unes a ellos, pero piensas que cada cual haga lo que quiera.'],
-      typeSpeed: 10,
-      showCursor:false,
-      onComplete: this.animateAnswer3
-    })
-  }
-
-  animateAnswer3(){
-    this.typed = new Typed('#writeText5', {
-      strings: [ 'Te unes a ellos y aprovechas esos privilegios sin darle muchas vueltas. Aunque no está bien, nadie te juzgará.'],
-      typeSpeed: 10,
       showCursor:false
     })
+
+    TextChooseComponent.animateAnswers();
   }
 
-  animateAnswers(){
-    console.log()
+  public static animateAnswers(){
+    setTimeout(()=>{
+      console.log('answerContainer1')
+      let answerContiner1 = document.getElementById('answerContainer1')
+      answerContiner1?.classList.remove('d-none')
+    },1500)
+    setTimeout(()=>{
+      console.log('answerContainer2')
+      let answerContiner2 = document.getElementById('answerContainer2')
+      answerContiner2?.classList.remove('d-none')
+    },2000)
+    setTimeout(()=>{
+      console.log('answerContainer3')
+      let answerContiner3 = document.getElementById('answerContainer3')
+      answerContiner3?.classList.remove('d-none')
+    },2500)
   }
 
 }
+  // animateAnswer1(){
+  //   this.typed = new Typed('#writeText3', {
+  //     strings: [ 'Te escandalizas e intentas hacer todo lo posible por evitar que se siga haciendo.'],
+  //     typeSpeed: 10,
+  //     showCursor:false,
+  //     onComplete: this.animateAnswer2
+  //   })
+  // }
+
+  // animateAnswer2(){
+  //   this.typed = new Typed('#writeText4', {
+  //     strings: [ 'No te unes a ellos, pero piensas que cada cual haga lo que quiera.'],
+  //     typeSpeed: 10,
+  //     showCursor:false,
+  //     onComplete: this.animateAnswer3
+  //   })
+  // }
+
+  // animateAnswer3(){
+  //   this.typed = new Typed('#writeText5', {
+  //     strings: [ 'Te unes a ellos y aprovechas esos privilegios sin darle muchas vueltas. Aunque no está bien, nadie te juzgará.'],
+  //     typeSpeed: 10,
+  //     showCursor:false
+  //   })
+  // }
+
+
+
+
